4d318e51adf2ab2ab33652632a0d4e4d25d7e915
shaikshakee <shaikshakeer123@gmail.com>
2025-05-27 11:48:46 +0530

    updated

--- data/server.js
+++ data/server.js
@@ -1,28 +1,89 @@
+// we  perform crud operations  using express framework
+
 const express = require('express');
 const app = express();
 const port = 3300;
 
-//enable json body parser
-//middleware can be used through use keyword
+// Enable JSON body parsing middleware
 
 app.use(express.json());
 
-let students = [
-    { id: 1, name: "Banu", course: "Cse"},
-    { id: 2, name: "hassu", course: "TET"}
-]
-app.get("/", (req,res) =>{
-    res.send(`<h1> Welcome to student management </h1>`);
-})
-app.get("/students",(req,res)=>{
-    res.json(students);
+// This is the array consisting of objects
 
-})
-app.get("/students/:id",(req,res)=>{
+let students = [
+    { id: 1, name: "Banu", course: "CSE" },
+    { id: 2, name: "Hassu", course: "TET" }
+];
+
+//This is the default page which consists of  a heading tag
+
+app.get("/", (req, res) => {
+    res.send(`Welcome to Student Management`);
+});
+
+//This is for displaying students data (using get() method)
+
+app.get("/students", (req, res) => {
+    res.json(students);     //in json format
+});
+
+// This is for displaying a particular person details using id using get() method
+
+app.get("/students/:id", (req, res) => {
     const id = parseInt(req.params.id);
     const student = students.find(s => s.id === id);
-    student ? res.json(student): res.status(404).send("student not found in records");
+    student
+        ? res.json(student)
+        : res.status(404).send("Student not found in records");
+});
+
+//This is for posting or inserting data into array of objects using post() method
+
+app.post("/students", (req, res) => {
+    const { name, course } = req.body;
+    const newStudent = {
+        id: students.length + 1,
+        name,
+        course
+    };
+    students.push(newStudent);
+    res.status(201).json(newStudent);
+
 })
-app.listen(port, ()=>{
-    console.log("running")
+
+// This is for updating data in array of objects using id particulary through put() method.
+
+app.put("/students/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const { name, course } = req.body;
+    const student = students.find(s => s.id === id);
+
+    if (student) {
+        student.name = name || student.name;
+        student.course = course || student.course;
+        res.json(student);
+    } else {
+        res.status(404).send("Student data not found");
+    }
+});
+
+// This is used for deleting data in an array of objects using particular id through delete() method
+
+app.delete("/students/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const index = students.findIndex(s => s.id === id);
+    if (index !== -1) {
+        const deleted = students.splice(index, 1);
+        res.json(deleted[0]);
+
+    } else {
+        res.status(404).send("deleted succesfully");
+    }
 })
+
+
+// listen() method for passing port so that can access in browser through localhost://
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
